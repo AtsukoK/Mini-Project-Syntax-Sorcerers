574d63a92792a8e858c8c0c84802b67f36bb9207
AtsukoK <lichungtai@hotmail.nl>
2023-09-11 19:01:54 +0200

    Monster class fields/init done
    Finished monster class,
    Still to do  METHODS.

--- Monster.cs
+++ Monster.cs
@@ -1,1 +1,18 @@
-test maria
+class Monster
+{
+    public int CurrentHitPoints;
+    public int ID;
+    public int MaximumDamage;
+    public int MaximumHitPoints
+    public string Name
+
+    public Monster(int id, int maximumdamage, int maximumhitpoints, string name)
+    {
+        ID = id;
+        MaximumDamage = maximumdamage
+        MaximumHitPoints = maximumhitpoints
+        Name = name
+    }
+
+
+}
