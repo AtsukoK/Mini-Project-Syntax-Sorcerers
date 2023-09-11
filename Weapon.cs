48def92ddf0aa1d291a1e22397c65186eae2b97f
AtsukoK <lichungtai@hotmail.nl>
2023-09-11 19:18:43 +0200

    Weapon class/construc done
    Left to do, methods

---
+++ Weapon.cs
@@ -0,0 +1,14 @@
+class Weapon
+{
+    public int ID;
+    public int MaximumDamage;
+    public string Name;
+
+    public Weapon(int id, int maximumdamage, string name)
+    {
+        ID = id;
+        MaximumDamage = maximumdamage;
+        Name = name;
+    }
+
+}
