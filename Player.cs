e98f6ab65bfb9cd766006b68ab3ea14171ecd79f
AtsukoK <lichungtai@hotmail.nl>
2023-09-11 19:49:33 +0200

    Player fields, construct done
    Left to do, methods etc

---
+++ Player.cs
@@ -0,0 +1,19 @@
+class Player
+{
+    public int CurrentHitPoints;
+    public int CurrentLocation;
+    public string CurrentWeapon;
+    public int MaximumHitPoints;
+    public string Name;
+
+    public Player(int currenthitpoints, int currentlocation, string currentweapon, int maximumhitpoints, string name)
+    {
+        CurrentHitPoints = currenthitpoints;
+        CurrentLocation = currentlocation;
+        CurrentWeapon = CurrentWeapon;
+        MaximumHitPoints = maximumhitpoints;
+        Name = name;
+    }
+
+
+}
