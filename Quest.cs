59fce1b873786aad59be93d087e9db6879c2924c
AtsukoK <lichungtai@hotmail.nl>
2023-09-11 19:11:00 +0200

    Quest fields /construc done
    Left to do, METHODS
    Creation of QUEST Objects
    Store quest objects in a list

---
+++ Quest.cs
@@ -0,0 +1,16 @@
+class Quest
+{
+    public string Discription;
+    public int ID;
+
+    public string Name;
+
+    public Quest(string discription, int id, string name)
+    {
+        Discription = discription;
+        ID = id;
+        Name = name;
+    }
+
+
+}
