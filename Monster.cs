6b72ca2153ad17353a07e85d037e7f1bd100ca33
sara444444 <1059593@hr.nl>
2023-09-12 21:56:51 +0200

    player methods damage en attack

--- Monster.cs
+++ Monster.cs
@@ -3,8 +3,8 @@ class Monster
     public int CurrentHitPoints;
     public int ID;
     public int MaximumDamage;
-    public int MaximumHitPoints
-    public string Name
+    public int MaximumHitPoints;
+    public string Name;
 
     public Monster(int id, string name, int maximumdamage, int maximumhitpoints, int currenthitpoints)
     {
@@ -13,8 +13,28 @@ class Monster
         MaximumDamage = maximumdamage;
         MaximumHitPoints = maximumhitpoints;
         CurrentHitPoints = currenthitpoints;
-
     }
 
+    public void MonsterTakeDamage(int damage)
+    {
+        CurrentHitPoints -= damage;
+        if (CurrentHitPoints < 0)
+        {
+            CurrentHitPoints = 0;
+        }
+    }
 
+    public void MonsterAttack(Player target)
+    {
+        int damage = CalculateMonsterDamage();
+        target.PlayerTakeDamage(damage);
+        Console.WriteLine($"{Name} attacks {target.Name}\n{damage} damage");
+    }
+
+    public int CalculateMonsterDamage()
+    {
+        Random random = new Random();
+        int damage = random.Next(10, 25);
+        return damage;
+    }
 }
--- Player.cs
+++ Player.cs
@@ -10,10 +10,38 @@ class Player
     {
         CurrentHitPoints = currenthitpoints;
         CurrentLocation = currentlocation;
-        CurrentWeapon = CurrentWeapon;
+        CurrentWeapon = currentweapon;
         MaximumHitPoints = maximumhitpoints;
         Name = name;
     }
 
+    public void PlayerTakeDamage(int damage)
+    {
+        CurrentHitPoints -= damage;
+        if (CurrentHitPoints < 0)
+        {
+            CurrentHitPoints = 0;
+        }
+    }
 
+    public void PlayerAttack(Monster target)
+    {
+        int baseDamage = CalculatePlayerDamage();
+        int totalDamage = baseDamage;
+
+        if (CurrentWeapon != 0)
+        {
+            totalDamage += 10;
+        }
+
+        target.MonsterTakeDamage(totalDamage);
+        Console.WriteLine($"{Name} attacks {target.Name}\n{totalDamage} damage");
+    }
+
+    public int CalculatePlayerDamage()
+    {
+        Random random = new Random();
+        int damage = random.Next(10, 25);
+        return damage;
+    }
 }
